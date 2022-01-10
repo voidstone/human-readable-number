e354814af7bb649e73b761fea46815840a8fef72
s.chaikin <s.chaikin@ra-wm.ru>
2022-01-10 12:45:32 +0300

    feat: add start numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const num = "zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" ");
+    const tens = "twenty thirty forty fifty sixty seventy eighty ninety".split(" ");
+
+
+    if (number < 20) return num[number];
+
 }
