4a12c84fab4a5bc556148fa94467ff65e1335184
s.chaikin <s.chaikin@ra-wm.ru>
2022-01-10 13:48:10 +0300

    feat: add func to toReadable bumber

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ module.exports = function toReadable(number) {
 
 
     if (number < 20) return num[number];
+    let digit = number % 10;
+    if (number < 100) return tens[Math.floor((number / 10)) - 2] + (digit ? " " + num[digit] : "");
+    if (number < 1000) return num[Math.floor(number / 100)] + " hundred" + (number % 100 == 0 ? "" : " " + toReadable(number % 100));
+    return toReadable(Math.floor(number / 1000)) + " thousand" + (number % 1000 != 0 ? " " + toReadable(number % 1000) : "");
 
 }
